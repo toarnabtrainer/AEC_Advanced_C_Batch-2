aee800ed765859651da6baa2f9860dc64ecd7832
Arnab Chakraborty <111301975+toarnabtrainer@users.noreply.github.com>
2023-09-25 15:45:00 +0530

    User Defined Functions

--- Funct2.c
+++ Funct2.c
@@ -1,26 +1,29 @@
 #include <stdio.h>
-#include <conio.h>
 
 int main(void) {
 	int num1, num2, result;
-	void my_welcome(void);   // prototype declaration
+	int my_input(void);    // prototype declaration
+	int my_addition(int, int);
+	void my_welcome(void);
 	void my_display(int, int, int);
 	void my_goodbye(void);
 	
 	my_welcome();
-	
-	printf("Please enter the first number: ");
-	scanf("%d", &num1);
-
-	printf("Please enter the second number: ");
-	scanf("%d", &num2);
-
-	printf("\nPerforming the ADDITION Operation...");
-	result = num1 + num2;
-	
+	num1 = my_input();
+	num2 = my_input();
+	result = my_addition(num1, num2);
 	my_display(num1, num2, result);
-	
 	my_goodbye();
+}
+int my_addition(int n1, int n2) {
+	printf("\nPerforming the ADDITION Operation...");
+	return n1 + n2;
+}
+int my_input(void) {
+	int number;
+	printf("Please enter one integer number: ");
+	scanf("%d", &number);
+	return number;
 }
 void my_goodbye(void) {
 	printf("\n\nEnd of the program...");
@@ -32,8 +35,5 @@ void my_display(int n1, int n2, int re) {
 void my_welcome(void) {
 	printf("\nWelcome to the WORLD of PROGRAMMING...\n");
 }
-
-
-
 
 
