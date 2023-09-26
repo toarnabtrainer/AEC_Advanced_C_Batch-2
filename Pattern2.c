af41a7b0fe2d62a176025eec1267452c4bc644ed
Arnab Chakraborty <111301975+toarnabtrainer@users.noreply.github.com>
2023-09-26 14:34:39 +0530

    Loop Control

--- Pattern2.c
+++ Pattern2.c
@@ -1,11 +1,11 @@
-
+/*
 User input n = 6
 				i   .   *
               -------------
 ***********     1   0  11       (n, i)
-.*********      2   1   9  . => (???)
+.*********      2   1   9  . => (i - 1)
 ..*******       3   2   7
-...*****        4   3   5  * => (???)
+...*****        4   3   5  * => (2 * (n - i) + 1)
 ....***         5   4   3
 .....*          6   5   1 
               -------------
@@ -17,8 +17,8 @@ int main(void) {
 	printf("\n\nPlease enter the number of layers: ");
 	scanf("%d", &n);
 	for(i = 1; i <= n; i++) {
-		for(j = 1; j <= (???); j++) printf(".");
-		for(j = 1; j <= (???); j++) printf("*");
+		for(j = 1; j <= (i - 1); j++) printf(".");
+		for(j = 1; j <= (2 * (n - i) + 1); j++) printf("*");
 		printf("\n");
 	}
 	printf("\n\nEnd of the pattern printing...");
